c16ff3416a57bb024124daa5dd119c806a2d9ef0
SK Rizwan <skrizwan1219@gmail.com>
2025-04-10 20:13:52 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+<h1>hello world</h1>
+<?php
+    echo 'Hello world';
+?>
+
+<?php
+    echo "<br>";
+    echo "<br>";
+
+    $variable1 = 99;
+    $variable2 = "tommy";
+    $variable3 = 33;
+    
+    echo $variable1;
+    echo "<br>";
+
+    echo $variable2;    
+    echo "<br>";
+    echo $variable1 + $variable3;
+?>
+
+<?php
+    // Operators
+?>
+
+</body>
+</html>
